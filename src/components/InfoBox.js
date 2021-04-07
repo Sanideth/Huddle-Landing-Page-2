94111aa98a8412e82f9b4635e5057e56246517ee
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-08 02:08:43 +0400

    info-box done, I think

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,19 +1,31 @@
-import Heading from './Heading';
-import Image from './Image';
-
+import Heading from "./Heading";
+import Image from "./Image";
 
 const InfoBox = (props) => {
-    return (
-        <div className={`${props.containerClassName}__info-box`}>
-            <div className={`${props.containerClassName}__text-container`}>
-                <Heading element={props.element} headingClassName={props.headingClassName} headingText={props.headingText} paragraphText={props.paragraphText} paragraphClassName={props.paragraphClassName}/>
-            </div>
-            <div className={`${props.containerClassName}__img-container`}>
-                <Image src={props.src} alt={props.alt} className={props.className} />
-            </div>
-        </div>
-        
-    )
-}
+  return (
+    <div className={`${props.containerClassName}__info-box`}>
+      <div
+        className={`${props.containerClassName}__text-container ${
+          props.reverse ? "u-o-2" : ""
+        }`}
+      >
+        <Heading
+          element={props.element}
+          headingClassName={props.headingClassName}
+          headingText={props.headingText}
+          paragraphText={props.paragraphText}
+          paragraphClassName={props.paragraphClassName}
+        />
+      </div>
+      <div
+        className={`${props.containerClassName}__img-container ${
+          props.reverse ? "u-o-1" : ""
+        }`}
+      >
+        <Image src={props.src} alt={props.alt} className={props.className} />
+      </div>
+    </div>
+  );
+};
 
 export default InfoBox;
