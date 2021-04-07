5415dc601a2fd5a5d308378e62e280784c849c5d
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-07 23:41:47 +0400

    Markup progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './layout/Header';
+import Main from './layout/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
