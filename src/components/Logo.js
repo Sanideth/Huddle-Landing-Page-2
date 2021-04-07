381a498ae63c8adcd50591a00a34796a9c1f10c9
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-07 22:26:22 +0400

    added component for Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import logoSrc from '../img/logo.svg'
+
+const Logo = () => {
+    return (
+        <a href="https://github.com/Sanideth/Huddle-Landing-Page-2" className="logo-link">
+            <img src={logoSrc} alt="Logo" className="logo"/>
+        </a>
+    )
+}
+
+
+export default Logo;
